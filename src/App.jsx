f6053fada1fdd8d9526c1d6a44ac57098c1cab1c
Maxwell Nathaniel Pramita <maxwellnathaniel385@gmail.com>
2026-05-31 01:06:12 +0700

    fix flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import logoGambar from "./assets/pinkandblue-removebg-preview.png";
 import {
   ShieldAlert,
